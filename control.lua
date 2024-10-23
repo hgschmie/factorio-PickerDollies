5519fd5c780a6ac160b4248219c09f9863fd6660
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2024-10-22 22:17:00 -0700

    [FACTORIO 2.0] Update available entity types

--- control.lua
+++ control.lua
@@ -33,19 +33,35 @@ end
 local copper_wire_types = array_to_dict { "electric-pole", "power-switch" }
 
 --- Entity types that can not be moved even in cheat_mode.
-local blacklist_types = array_to_dict { "item-request-proxy", "rocket-silo-rocket", "resource", "construction-robot",
-    "logistic-robot", "rocket", "tile-ghost", "item-entity", "straight-rail", "curved-rail", "locomotive", "cargo-wagon",
-    "artillery-wagon", "fluid-wagon"
+local blacklist_types = array_to_dict {
+
+    -- rails and train stuff
+    "straight-rail", "half-diagonal-rail", "curved-rail-a", "curved-rail-b", "legacy-straight-rail", "legacy-curved-rail",
+    "elevated-curved-rail-a", "elevated-curved-rail-b", "elevated-half-diagonal-rail", "elevated-straight-rail",
+    "rail-ramp", "rail-support", "train-stop", "rail-signal", "rail-chain-signal", "rail-remnants",
+    "locomotive", "cargo-wagon", "artillery-wagon", "fluid-wagon",
+    -- robots
+    "construction-robot", "logistic-robot", "combat-robot",
+    -- rockets and space stuff
+    "rocket-silo-rocket", "rocket-silo-rocket-shadow", "cargo-landing-pad", "cargo-pod",
+    -- belts and containers
+    "linked-belt", "underground-belt", "temporary-container",
+    -- environment
+    "cliff", "tree", "resource", "explosion", "particle-source", "fire", "sticker", "stream", "beam", "artillery-flare", "projectile",
+    -- internal stuff
+    "item-request-proxy", "tile-ghost", "item-entity", "deconstructible-tile-proxy", "arrow", "highlight-box", "entity-ghost", "speech-bubble", "smoke-with-trigger",
+    -- misc
+    "spider-leg",
 }
 
 --- Entity types that can only be moved in cheat_mode.
-local blacklist_cheat_types = array_to_dict { "character", "unit", "unit-spawner", "car", "spider-vehicle" }
+local blacklist_cheat_types = array_to_dict { "character", "unit", "unit-spawner", "car", "spider-vehicle", "simple-entity", "corpse", "character-corpse" }
 
 --- Default entity names to blacklist from moving. Stored in global and can be modified by the user via interface.
 local blacklist_names = array_to_dict { "pumpjack" }
 
 --- Default entity names with none-square bounding boxes. Stored in global and can be modified by the user via interface.
-local oblong_names = array_to_dict { "arithmetic-combinator", "decider-combinator", "pump" }
+local oblong_names = array_to_dict { "pump", "arithmetic-combinator", "decider-combinator", "selector-combinator", }
 
 local input_to_direction = {
     ["dolly-move-north"] = defines.direction.north,
